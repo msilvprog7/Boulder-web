4650a25214edeae825d6231b88d52abb793c3531
Hunter Leath <h@hunterleath.com>
2014-03-01 17:16:26 -0500

    Fixed Index URL

--- boulder/urls.py
+++ boulder/urls.py
@@ -6,7 +6,7 @@ from main import controls
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^', controls.Home.as_view()),
+	url(r'^$', controls.Home.as_view()),
 	url(r'^boulder/', include('main.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
