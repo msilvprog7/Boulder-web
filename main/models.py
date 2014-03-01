4fe5664cd9ea2977831533fc732cfdbc3d1e676f
Michael Snider <msilvprog7@gmail.com>
2014-03-01 16:11:51 -0500

    Added base models for Activity and CompletedActivity

--- main/models.py
+++ main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Activity(models.Model):
+	""" Activity stored in the database to associate with specific motions
+		with the pebble
+	"""
+	name = models.CharField(max_length=255)
+	points = models.IntegerField(default=0)
+	description = models.TextField()
+
+	@staticmethod
+	def getUserPoints(user_id, activity_id=-1):
+		""" Returns the number of points earned by the user for each CompletedActivity
+		"""
+		completed_activities = CompletedActivity.objects.filter(user=user_id)
+
+		if activity_id >= 0:
+			completed_activities = completed_activities.filter(activity=activity_id)
+
+		sum = 0
+		for current_activity in completed_activities:
+			sum += Activity.objects.get(id=current_activity.activity_id).points
+
+		return sum
+
+	def getActivityUserPoints(self, user_id):
+		""" Returns number of points earned by the user for the current activity
+		"""
+		return Activity.getUserPoints(user_id, self.id)
+
+
+
+class CompletedActivity(models.Model):
+	activity = models.ForeignKey(Activity)
+	user = models.ForeignKey(User)
+	time = models.TimeField(auto_now_add=True)
