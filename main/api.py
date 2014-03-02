47e883c30e8241ec57f36bebdcc7b8d8cbcc1d9a
Hunter Leath <h@hunterleath.com>
2014-03-02 03:42:13 -0500

    Added MethodDecorator

--- main/api.py
+++ main/api.py
@@ -2,6 +2,7 @@ import json
 from django.http import HttpResponse
 from django.views.generic import View
 
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 from main.models import PebbleToken
